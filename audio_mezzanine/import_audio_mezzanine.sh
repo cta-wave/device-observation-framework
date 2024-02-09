58c04e4fc70f06f63a2fc3654483f5de0b994803
Yan Jiang <yan.jiang@edt.zone>
2024-02-09 10:45:05 +0000

    re-add import_audio_mezzanine script.

---
+++ audio_mezzanine/import_audio_mezzanine.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# import mezzanine_database.json
+wget -O mezzanine_database.json https://raw.githubusercontent.com/cta-wave/mezzanine/master/metadata/audio_mezzanine_database.json
+
+# download audio mezzanine files
+python download_mezzanine.py
